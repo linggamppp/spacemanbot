cdb908d06ed51cf4a477d9c64610bde582391e1c
linggamppp <76404832+linggamppp@users.noreply.github.com>
2025-07-11 06:42:10 +0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -4,20 +4,12 @@ import os
 
 async def download_proof(context, file_id, user_id, ext="jpg"):
     try:
-        # Dapatkan file dari Telegram
         file = await context.bot.get_file(file_id)
-
-        # Pastikan folder bukti_transfer ada
         folder = "bukti_transfer"
         os.makedirs(folder, exist_ok=True)
-
-        # Tentukan nama file
         filename = f"{folder}/user_{user_id}_{file_id}.{ext}"
-
-        # Download file ke drive
         await file.download_to_drive(filename)
         print(f"✅ Bukti disimpan: {filename}")
-
         return filename
     except Exception as e:
         print(f"❌ Gagal mengunduh file: {e}")
